3c6c85a3930bdfb33abc6626f2167e3f915cf360
Dayahyd <dayakarhero4@gmail.com>
2025-09-28 17:03:11 +0530

    git

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# Devops-30Days" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Dayahyd/Devops-30Days.git
+git push -u origin main

0b838eae53f3f6d547494f448f49541689c61c13
Dayahyd <dayakarhero4@gmail.com>
2025-10-02 23:09:56 +0530

    spring

---
+++ demo/demo/src/main/java/com/example/demo/Dev.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    @Autowired
+    @Qualifier("desktop")
+    private Computer comp; // filed Injection
+
+//    public Dev(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
+
+
+//    @Autowired
+//    public void setLaptop(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
+
+    public void  build(){
+        comp.compile();
+        System.out.println("daya");
+
+    }
+}
